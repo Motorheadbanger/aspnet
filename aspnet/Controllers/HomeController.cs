3463a1a873c05977c6de597cab25590a86af30b5
Motorheadbanger <dv.fominykh@gmail.com>
2019-12-11 10:25:22 +0300

    Add project files.

---
+++ aspnet/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using aspnet.Models;
+
+namespace aspnet.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Top of the morning!" : "G'day";
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guest)
+        {
+            return View("Thanks", guest);
+        }
+    }
+}
