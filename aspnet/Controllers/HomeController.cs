72924c37d47284f1adc82dba4a0b35b11c05df1d
Motorheadbanger <dv.fominykh@gmail.com>
2020-01-24 02:39:53 +0300

    v

--- aspnet/Controllers/HomeController.cs
+++ aspnet/Controllers/HomeController.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using aspnet.Models;
 
@@ -26,7 +23,7 @@ namespace aspnet.Controllers
         [HttpPost]
         public ViewResult RsvpForm(GuestResponse guest)
         {
-                return View("Thanks", guest);
+            return View("Thanks", guest);
         }
     }
 }
--- aspnet/Models/GuestResponse.cs
+++ aspnet/Models/GuestResponse.cs
@@ -8,17 +8,12 @@ namespace aspnet.Models
 {
     public class GuestResponse
     {
-        [Required(ErrorMessage="Please, submit the name")]
         public string Name { get; set; }
 
-        [Required(ErrorMessage = "Please, submit the e-mail address")]
-        [RegularExpression(".+\\@.+\\..+", ErrorMessage="Incorrect e-mail")]
         public string Email { get; set; }
 
-        [Required(ErrorMessage = "Please, submit the phone number")]
         public string Phone { get; set; }
 
-        [Required(ErrorMessage = "Please, submit your decision")]
         public bool? WillAttend { get; set; }
     }
 }
